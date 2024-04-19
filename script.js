f8e1248a849e13020acc2afde68a131a39f6edae
unknown <rizqullahyusufrafly@gmail.com>
2024-04-19 10:14:47 +0700

    update terbaru

---
+++ script.js
@@ -0,0 +1,16 @@
+// harus pakai button ternyata wkwkwk
+
+function redirInstagram() {
+    window.location.href = 
+    "https://instagram.com/rfly_rzqllh"
+}
+
+function redirWhatsapp() {
+    window.location.href = 
+    "https://wa.me/+083135852840"
+}
+
+const redirGmail = () =>{
+    window.location.href = 
+    "mailto:rizqullahyusufrafly@gmail.com"
+}
